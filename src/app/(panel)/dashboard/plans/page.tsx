883acbe58ffb78d16f23b67bd1189b6d07e2029e
Marcio Jorge Medeiros Melo Filho <marcio.melo@livsaude.com.br>
2025-03-28 11:49:12 -0300

    feat: create estructure´s dashboard page and your acessable pages

---
+++ src/app/(panel)/dashboard/plans/page.tsx
@@ -0,0 +1,7 @@
+export default function Plans() {
+  return (
+    <div>
+      <h1>Página planos</h1>
+    </div>
+  );
+}
---
+++ src/app/(panel)/dashboard/profile/page.tsx
@@ -0,0 +1,7 @@
+export default function Profile() {
+  return (
+    <div>
+      <h1>Página profile</h1>
+    </div>
+  );
+}
