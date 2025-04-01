ca23437bcb79510c72f4cb0b4fb8893cb75480e0
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-04-01 16:11:53 -0300

    feat: add verify auth and receive data´s logged user on page

--- src/app/(panel)/dashboard/profile/page.tsx
+++ src/app/(panel)/dashboard/profile/page.tsx
@@ -1,4 +1,20 @@
-export default function Profile() {
+import { getSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+import { getUserData } from "./_data-access/get-info-user";
+
+export default async function Profile() {
+  const session = await getSession();
+
+  if (!session) {
+    redirect("/");
+  }
+
+  const user = await getUserData({ userId: session?.user?.id });
+
+  if (!user) {
+    redirect("/");
+  }
+
   return (
     <div>
       <h1>Página profile</h1>
