3c0ed807229f7f060d9c083b3287f39b63e99407
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-04-11 17:23:43 -0300

    feat: add scrollArea´s shadcn round lembretes map and use server action to delete reminder

--- src/app/(panel)/dashboard/_components/reminder/reminder-list.tsx
+++ src/app/(panel)/dashboard/_components/reminder/reminder-list.tsx
@@ -2,14 +2,31 @@
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { Reminder } from "@prisma/client";
 import { Plus, Trash } from "lucide-react";
+import { deleteReminder } from "../../_actions/delete-reminder";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 interface ReminderListProps {
   reminders: Reminder[];
 }
 
 export function ReminderList({ reminders }: ReminderListProps) {
+  const router = useRouter();
+
+  async function handleDeleteReminder(id: string) {
+    const response = await deleteReminder({ reminderId: id });
+
+    if (response.error) {
+      toast.error(response.error);
+    }
+
+    toast.success(response.data);
+    router.refresh();
+  }
+
   return (
     <div className="flex flex-col gap-3">
       <Card>
@@ -30,21 +47,24 @@ export function ReminderList({ reminders }: ReminderListProps) {
             </p>
           )}
 
-          {reminders.map((item) => (
-            <article
-              className="flex flex-wrap flex-row items-center justify-between py-2 bg-yellow-100 mb-2 px-2 rounded-md"
-              key={item.id}
-            >
-              <p className="text-sm lg:text-base">{item.description}</p>
-
-              <Button
-                className="bg-red-500 hover:bg-red-400 shadow-none rounded-full p2"
-                size="sm"
+          <ScrollArea className="h-[340px] lg:max-h-[calc(100vh-15rem)] pr-0 w-full flex-1">
+            {reminders.map((item) => (
+              <article
+                className="flex flex-wrap flex-row items-center justify-between py-2 bg-yellow-100 mb-2 px-2 rounded-md"
+                key={item.id}
               >
-                <Trash className="w-4 h-4 text-white" />
-              </Button>
-            </article>
-          ))}
+                <p className="text-sm lg:text-base">{item.description}</p>
+
+                <Button
+                  className="bg-red-500 hover:bg-red-400 shadow-none rounded-full p2"
+                  size="sm"
+                  onClick={() => handleDeleteReminder(item.id)}
+                >
+                  <Trash className="w-4 h-4 text-white" />
+                </Button>
+              </article>
+            ))}
+          </ScrollArea>
         </CardContent>
       </Card>
     </div>
