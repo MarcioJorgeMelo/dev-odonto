7f097a2ed82be894a52704411fae6fd23b4a0580
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-04-11 17:03:36 -0300

    feat: create reminder component to receive data´s server action and pass to reminderList component renderize

---
+++ src/app/(panel)/dashboard/_components/reminder/reminder-list.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Reminder } from "@prisma/client";
+import { Plus, Trash } from "lucide-react";
+
+interface ReminderListProps {
+  reminders: Reminder[];
+}
+
+export function ReminderList({ reminders }: ReminderListProps) {
+  return (
+    <div className="flex flex-col gap-3">
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-xl md:text-2xl font-bold">
+            Lembretes
+          </CardTitle>
+
+          <Button variant="ghost" className="w-9 p-0">
+            <Plus className="w-5 h-5" />
+          </Button>
+        </CardHeader>
+
+        <CardContent>
+          {reminders.length === 0 && (
+            <p className="text-sm text-gray-500">
+              Nenhum lembrete registrado...
+            </p>
+          )}
+
+          {reminders.map((item) => (
+            <article
+              className="flex flex-wrap flex-row items-center justify-between py-2 bg-yellow-100 mb-2 px-2 rounded-md"
+              key={item.id}
+            >
+              <p className="text-sm lg:text-base">{item.description}</p>
+
+              <Button
+                className="bg-red-500 hover:bg-red-400 shadow-none rounded-full p2"
+                size="sm"
+              >
+                <Trash className="w-4 h-4 text-white" />
+              </Button>
+            </article>
+          ))}
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
