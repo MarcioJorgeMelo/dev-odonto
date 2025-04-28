559ef77e346e66926173490d5d1a89730a14888e
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-04-28 16:47:53 -0300

    feat: create style to open appointment

--- src/app/(panel)/dashboard/_components/appointments/appointment-list.tsx
+++ src/app/(panel)/dashboard/_components/appointments/appointment-list.tsx
@@ -1,6 +1,14 @@
 "use client";
 
 import { useRouter, useSearchParams } from "next/navigation";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export function AppointmentList({ times }: { times: string[] }) {
   const searchParams = useSearchParams();
@@ -9,8 +17,31 @@ export function AppointmentList({ times }: { times: string[] }) {
   console.log(date);
 
   return (
-    <div>
-      <h1>Listagem de horários...</h1>
-    </div>
+    <Card>
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-xl md:text-2xl font-bold">
+          Agendamentos
+        </CardTitle>
+
+        <button>Selecionar data</button>
+      </CardHeader>
+
+      <CardContent>
+        <ScrollArea className="h-[calc(100vh-20rem)] lg:h-[calc(100vh-15rem)] pr-4">
+          {times.map((slot) => {
+            return (
+              <div
+                key={slot}
+                className="flex items-center py-2 border-t last:border-b"
+              >
+                <div className="w-16 text-sm font-semibold">{slot}</div>
+
+                <div className="flex-1 text-sm text-gray-500">Disponível</div>
+              </div>
+            );
+          })}
+        </ScrollArea>
+      </CardContent>
+    </Card>
   );
 }
