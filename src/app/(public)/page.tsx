cc2411eecb56a159142ba6b36d13fa5773605c58
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-04-29 17:05:34 -0300

    feat: await server action and pass to Clinics component to renderize

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -2,8 +2,11 @@ import { Clinics } from "./_components/clinics";
 import { Footer } from "./_components/footer";
 import { Header } from "./_components/header";
 import { Hero } from "./_components/hero";
+import { getClinics } from "./_data-access/get-clinics";
 
-export default function Home() {
+export default async function Home() {
+  const clinics = await getClinics();
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
@@ -11,7 +14,7 @@ export default function Home() {
       <div>
         <Hero />
 
-        <Clinics />
+        <Clinics clinics={clinics || []} />
 
         <Footer />
       </div>
