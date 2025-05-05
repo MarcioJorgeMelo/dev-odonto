5020bac3c5599865eef76a8ceaa832adc96bc42c
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-05-05 18:18:38 -0300

    feat: add gridPlans component on Plans page

--- src/app/(panel)/dashboard/plans/page.tsx
+++ src/app/(panel)/dashboard/plans/page.tsx
@@ -1,7 +1,17 @@
-export default function Plans() {
+import getSession from "@/lib/getSession";
+import { redirect } from "next/navigation";
+import { GridPlans } from "./_components/grid-plans";
+
+export default async function Plans() {
+  const session = await getSession();
+
+  if (!session) {
+    redirect("/");
+  }
+
   return (
     <div>
-      <h1>Página planos</h1>
+      <GridPlans />
     </div>
   );
 }
