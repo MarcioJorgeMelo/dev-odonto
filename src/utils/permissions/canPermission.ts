8bcec0eb470f33cef0a4757d605680578f57812c
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-05-07 20:42:55 -0300

    feat: create function to check if you can permission to call some functions

---
+++ src/utils/permissions/canPermission.ts
@@ -0,0 +1,55 @@
+"use server";
+
+import { auth } from "@/lib/auth";
+import { PlanDetailInfo } from "./get-plan";
+import prisma from "@/lib/prisma";
+import { canCreateService } from "./canCreateService";
+
+export type PLAN_PROP = "BASIC" | "PROFESSIONAL" | "TRIAL" | "EXPIRED";
+
+export interface ResultPermissionProps {
+  hasPermission: boolean;
+  planId: PLAN_PROP;
+  expired: boolean;
+  plan: PlanDetailInfo | null;
+}
+
+interface CanPermissionProps {
+  type: string;
+}
+
+export async function canPermission({
+  type,
+}: CanPermissionProps): Promise<ResultPermissionProps> {
+  const session = await auth();
+
+  if (!session?.user?.id) {
+    return {
+      hasPermission: false,
+      planId: "EXPIRED",
+      expired: true,
+      plan: null,
+    };
+  }
+
+  const subscription = await prisma.subscription.findFirst({
+    where: {
+      userId: session.user.id,
+    },
+  });
+
+  switch (type) {
+    case "service":
+      const permissions = await canCreateService(subscription, session);
+
+      return permissions;
+
+    default:
+      return {
+        hasPermission: false,
+        planId: "EXPIRED",
+        expired: true,
+        plan: null,
+      };
+  }
+}
