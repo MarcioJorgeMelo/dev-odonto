a5355f51e05a1fe832208efba63a09d77a27e82e
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-05-07 21:16:04 -0300

    feat: create a alert if plan has expired or closed time´s trial plan

--- src/app/(panel)/dashboard/services/_components/service-content.tsx
+++ src/app/(panel)/dashboard/services/_components/service-content.tsx
@@ -1,6 +1,7 @@
 import { canPermission } from "@/utils/permissions/canPermission";
 import { getAllServices } from "../_data-access/get-all-services";
 import { ServiceList } from "./service-list";
+import { LabelSubscription } from "@/components/ui/label-subscription";
 
 interface ServicesContentProps {
   userId: string;
@@ -11,5 +12,13 @@ export async function ServicesContent({ userId }: ServicesContentProps) {
 
   const permission = await canPermission({ type: "service" });
 
-  return <ServiceList services={services.data || []} permission={permission} />;
+  return (
+    <>
+      {!permission.hasPermission && (
+        <LabelSubscription expired={permission.expired} />
+      )}
+
+      <ServiceList services={services.data || []} permission={permission} />
+    </>
+  );
 }
